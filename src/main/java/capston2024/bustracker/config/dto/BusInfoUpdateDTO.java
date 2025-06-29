d24aecab070cd76351f73c575dcb0779cfa1e42a
taehun2123 <pyoneng_@naver.com>
2025-06-29 14:52:14 +0900

    update: staff 대시보드 수정

--- src/main/java/capston2024/bustracker/config/dto/BusInfoUpdateDTO.java
+++ src/main/java/capston2024/bustracker/config/dto/BusInfoUpdateDTO.java
@@ -9,6 +9,6 @@ public class BusInfoUpdateDTO {
     private String busNumber;       // 버스 번호 (3~6자리 고유 코드)
     private String busRealNumber;   // 실제 버스 번호 (운영자가 지정하는 번호)
     private String routeId;         // 라우트 ID
-    private int totalSeats;         // 전체 좌석 수
+    private Integer totalSeats;      // 전체 좌석 수 (Integer로 변경)
     private Boolean isOperate;      // 운행 여부 (Boolean으로 null 허용)
 }
